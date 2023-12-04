0ac9efe058dfea41c72120d546576ae48395a4c1
mSharifHub <mosharifemail@gmail.com>
2023-12-04 04:59:42 -0800

    #1.3 fixing the style for the display by category

--- src/components/categoryDisplay/CategoryDisplay.jsx
+++ src/components/categoryDisplay/CategoryDisplay.jsx
@@ -1,28 +1,42 @@
 import { useCart } from "../../providers/contexts/cartContext";
 import { Product } from "../products/Product";
 import { filterCategory } from "../filter/FilterCategory";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 export const CategoryDisplay = () => {
     const {
         state: { products },
     } = useCart();
 
-    const categories = filterCategory(products);
+    const slider = (category, direction) => {
+        var slider = document.getElementById(`slider-${category}`);
+        const scrollAmount = 500;
 
+        switch (direction) {
+            case "left":
+                slider.scrollLeft -= scrollAmount;
+                break;
+            case "right":
+                slider.scrollLeft += scrollAmount;
+                break;
+            default:
+                return null;
+        }
+    };
+
+    const categories = filterCategory(products);
     return (
         <>
             {Object.keys(categories).map((category, index) => (
                 <>
-                    <div key={index} className="flex flex-col  items-start mb-10   bg-red-500">
-                        <div className="w-full text-center text-xl font-bold p-2 bg-green-700">{category.toUpperCase()}</div>
-
-                        <div className="relative flex items-center w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth">
-                            {categories[category].map((product) => (
-                                <div key={product.id}>
-                                    <Product product={product} />
-                                </div>
-                            ))}
-                        </div>
+                    <div>{category.toUpperCase()}</div>
+                    <MdChevronLeft onClick={() => slider(category, "left")} size={40}  />
+                    <div key={index} id={`slider-${category}`}className="relative p-4 flex items-center w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth ">
+                        {categories[category].map((product) => (
+                            <div key={product.id}>
+                                <Product product={product} />
+                            </div>
+                        ))}
                     </div>
                 </>
             ))}
