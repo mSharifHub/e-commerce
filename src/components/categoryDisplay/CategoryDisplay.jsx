44391e84fae367e9643cefaa63ab2dfa6b436841
mSharifHub <mosharifemail@gmail.com>
2023-12-05 01:08:21 -0800

    #1.4 display by category fixed

--- src/components/categoryDisplay/CategoryDisplay.jsx
+++ src/components/categoryDisplay/CategoryDisplay.jsx
@@ -27,34 +27,40 @@ export const CategoryDisplay = () => {
     const categories = filterCategory(products);
     return (
         <>
-            {Object.keys(categories).map((category, index) => (
-                <>
-                    <div className="text-center w-full mx-8 my-8">{category.toUpperCase()}</div>
-                    <div className="relative">
-                        <div className="absolute z-10 px-2 py-6 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 hover:cursor-pointer opacity-0  hover:opacity-75 duration-150 ease-in-out  bg-slate-200">
-                            <MdChevronLeft onClick={() => slider(category, "left")} size={40} className="justify-items-center " />
+            <div className="relative h-full w-full p-4 m-4 ">
+                {Object.keys(categories).map((category, index) => (
+                    <>
+                        <div className="text-center p-4  text-lg tracking-wide">{category.toUpperCase()}</div>
+                        <div className="relative">
+                            <div className="absolute z-50 px-2 py-6 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 hover:cursor-pointer opacity-0  hover:opacity-75 duration-150 ease-in-out  bg-slate-200">
+                                <MdChevronLeft
+                                    onClick={() => slider(category, "left")}
+                                    size={40}
+                                    className="justify-items-center "
+                                />
+                            </div>
+                            <div
+                                key={index}
+                                id={`slider-${category}`}
+                                className="relative  flex items-center w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide gap-4 "
+                            >
+                                {categories[category].map((product) => (
+                                    <div key={product.id}>
+                                        <Product product={product} />
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="absolute z-50 px-2 py-6 text-center transform  right-0 translate-x-1/2 -translate-y-1/2 top-1/2  hover:cursor-pointer opacity-0  hover:opacity-75 duration-150 ease-in-out  bg-slate-200">
+                                <MdChevronRight
+                                    onClick={() => slider(category, "right")}
+                                    size={40}
+                                    className="justify-items-center"
+                                />
+                            </div>
                         </div>
-                        <div
-                            key={index}
-                            id={`slider-${category}`}
-                            className="relative  flex items-center w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide "
-                        >
-                            {categories[category].map((product) => (
-                                <div key={product.id}>
-                                    <Product product={product} />
-                                </div>
-                            ))}
-                        </div>
-                        <div className="absolute z-10 px-2 py-6 text-center transform  right-0 translate-x-1/2 -translate-y-1/2 top-1/2  hover:cursor-pointer opacity-0  hover:opacity-75 duration-150 ease-in-out  bg-slate-200">
-                            <MdChevronRight
-                                onClick={() => slider(category, "right")}
-                                size={40}
-                                className="justify-items-center"
-                            />
-                        </div>
-                    </div>
-                </>
-            ))}
+                    </>
+                ))}
+            </div>
         </>
     );
 };
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,8 +4,15 @@ import { Filter } from "../filter/Filter";
 export const Home = () => {
     return (
         <>
-            <div className="m-10 ">
-                <CategoryDisplay />
+            <div className=" relative w-screen  h-screen  grid grid-cols-[300px_1fr]">
+                <div className="z-0 bg-green-300">
+                    <Filter />
+                </div>
+                <div className="h-full w-full overflow-x-scroll z-10 scroll whitespace-nowrap">
+                    <div className="z-20 ">
+                        <CategoryDisplay />
+                    </div>
+                </div>
             </div>
         </>
     );
