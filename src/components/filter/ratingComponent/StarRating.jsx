97470d9aaf987e066e58655ebacf45d436040132
mSharifHub <mosharifemail@gmail.com>
2023-12-10 02:51:43 -0800

    jsx-a11y/label-has-associated-control configuration added allowChildren to true fixed the error on the StarRating mapping

--- src/components/filter/ratingComponent/StarRating.jsx
+++ src/components/filter/ratingComponent/StarRating.jsx
@@ -5,11 +5,15 @@ export default function StarRating() {
   return (
     <div>
       {[...Array(5)].map((star, index) => {
-        const starId = `start-rating-${index + 1}`;
+        const starID = `star-rating-${index + 1}`;
         return (
-          <label htmlFor={starId}>
-            <input name="star-rating" id={starId} type="radio" />
-            <FontAwesomeIcon icon={faStar} size="lg" />
+          <label htmlFor={starID}>
+            <input id={starID} name="star-rating" type="radio" />
+            <FontAwesomeIcon
+              icon={faStar}
+              size="lg"
+              className="cursor-pointer"
+            />
           </label>
         );
       })}
