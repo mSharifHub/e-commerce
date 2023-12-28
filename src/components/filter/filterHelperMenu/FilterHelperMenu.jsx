435b139697caad16f0e587fe56d39f640d67194c
mSharifHub <msharifemail@gmail.com>
2023-12-28 15:33:28 -0800

    #Refractor#1.5 changed component modal prop drill style to resuse in different style sets

--- src/components/filter/filterHelperMenu/FilterHelperMenu.jsx
+++ src/components/filter/filterHelperMenu/FilterHelperMenu.jsx
@@ -14,10 +14,7 @@ export default function FilterHelperModal({ label, Component, modalStyle }) {
   };
 
   const modal = (
-    <div
-      className={`fixed inset-x-0  h-1/2  top-[8rem] flex justify-center items-start md:top-[6.5rem] z-50 bg-white shadow-lg rounded-lg
-      }`}
-    >
+    <div className={modalStyle}>
       <Component />
     </div>
   );
--- src/components/navigation/navigation_helpers/NavHelper.jsx
+++ src/components/navigation/navigation_helpers/NavHelper.jsx
@@ -1,3 +1,4 @@
+import { modalStyles } from "../../../helpers/navigation_helpers/modalStyle";
 import FilterHelperModal from "../../filter/filterHelperMenu/FilterHelperMenu";
 import CartIcon from "../icons/CartIcon";
 import HeartIcon from "../icons/HeartIcon";
@@ -6,13 +7,27 @@ import DiscountModal from "./navigation_helpers_modals/DiscountModal";
 import NewProductModal from "./navigation_helpers_modals/NewProductModal";
 import OpenBoxProductModal from "./navigation_helpers_modals/OpenBoxProductModal";
 
+const { full } = modalStyles;
+
 export default function NavHelper() {
   return (
     <div className="grid  grid-cols-1  md:grid-cols-[1fr_0.2fr] ">
       <div className="flex justify-center items-center space-x-12  col-start-1 capitalize  whitespace-nowrap transition-transform duration-500 sm:translate-x-0 -translate-x-full">
-        <FilterHelperModal label="50% off" Component={DiscountModal} />
-        <FilterHelperModal label="new products" Component={NewProductModal} />
-        <FilterHelperModal label="open box" Component={OpenBoxProductModal} />
+        <FilterHelperModal
+          label="50% off"
+          Component={DiscountModal}
+          modalStyle={full}
+        />
+        <FilterHelperModal
+          label="new products"
+          Component={NewProductModal}
+          modalStyle={full}
+        />
+        <FilterHelperModal
+          label="open box"
+          Component={OpenBoxProductModal}
+          modalStyle={full}
+        />
       </div>
       <div className="flex -translate-y-1/2 mt-4 mr-4 justify-center md:justify-end items-center  space-x-5 col-start-1 md:col-start-2  transition-all duration-500 sm:translate-y-0">
         <SearchBar />
