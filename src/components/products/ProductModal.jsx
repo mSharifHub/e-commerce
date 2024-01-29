2dd1f73a6cc95b6641c628b5850a85be6fb92939
mSharifHub <msharifemail@gmail.com>
2024-01-28 23:45:22 -0800

    #1.8 #Refractored  UI

--- src/components/products/ProductModal.jsx
+++ src/components/products/ProductModal.jsx
@@ -9,7 +9,7 @@ export default function ProductModal({ product, setOnClose }) {
         {/* inner content frame */}
         <div className=" relative w-[95%] h-[95%] grid  grid-cols-1   xl:grid-cols-[1fr_0.5fr] bg-white rounded-xl ">
           {/* column left */}
-          <div className=" relative flex flex-col flex-1  col-start-1 col-span-1 justify-center items-center">
+          <div className=" relative flex flex-col flex-1  col-start-1 col-p-1 justify-center items-center">
             {/* product frame */}
             <div className=" w-[400px] h-[400px] sm:w-[600px] sm:h-[600px]  bg-white grid grid-cols-1 rounded-lg shadow-lg transition-all duration-100 ease-out">
               <div className=" flex justify-center items-center ">image</div>
@@ -19,57 +19,75 @@ export default function ProductModal({ product, setOnClose }) {
           {/* end column left */}
 
           {/* column right */}
-          <div className="col-start-1 xl:col-start-2 col-span-1 overflow-y-auto scroll-smooth whitespace-nowrap max-h-[90%] w-full">
+          <div className="col-start-1 xl:col-start-2 col-p-1 overflow-y-auto scroll-smooth  max-h-[90%] w-full">
             {/* inner content */}
-            <div className="h-full grid grid-rows-1 xl:grid-rows-[1fr_0.5fr_1fr]">
+            <div className="h-full grid grid-rows-1 xl:grid-rows-[1fr_0.5fr_1fr_0.25fr]">
               {/* Product details */}
-              <div className="flex  flex-col justify-start items-center row-start-1 row-span-1 mt-4 ">
-                <span> {product.name}</span>
-                <span> {product.color}</span>
-                <span> {product.category}</span>
-                <span>{product.price}</span>
+              <div className="h-full row-start-1 row-span-1 mt-4 mx-4">
+                <div className="flex flex-col justify-between ">
+                  <div className="flex  flex-col justify-start items-center font-thin font-sans tracking-wider ">
+                    <span className="text-xl">{product.name}</span>
+                    <span className="text-xl uppercase font-medium text-neutral-600 ">{`"${product.color}"`}</span>
+                    <span className="text-md  font-semibold text-neutral-500">
+                      {product.category}
+                    </span>
+                    <span className="mt-4 font-bold text-md text-neutral-500">
+                      {product.price}
+                    </span>
+                  </div>
+
+                  <div className="flex flex-1  text-balance  mt-4">
+                    <p className="font-thin text-lg">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Est doloremque voluptatem veritatis ipsum, consequatur,
+                      sint quas placeat suscipit similique, omnis officiis quia.
+                      Exercitationem sequi qui itaque veniam debitis, fugit
+                      perspiciatis dolores molestiae laborum quasi tenetur
+                      aliquid officia alias beatae! Fuga voluptatibus rerum sit.
+                      Sunt aperiam maiores vitae maxime, voluptatibus corporis.
+                    </p>
+                  </div>
+                </div>
               </div>
               {/* End Product details  */}
 
               {/* shopping cart and favorites */}
-              <div className=" flex flex-col justify-center items-center row-start-1 xl:row-start-2  row-span-1 space-y-10">
+              <div className=" flex flex-col mt-8 justify-center items-center row-start-1 xl:row-start-2  row-span-1 space-y-10">
                 <button
                   type="button"
-                  className=" w-[20rem] h-[4rem] bg-black text-white rounded-xl text-lg font-thin font-mono capitalize transition-all duration-100 hover:scale-105"
+                  className=" w-[20rem] h-[4rem] bg-black text-white rounded-xl text-lg font-thin  capitalize transition-all duration-100 hover:scale-105"
                   onClick={setOnClose}
                 >
                   Add To cart
                 </button>
                 <button
                   type="button"
-                  className=" w-[20rem] h-[4rem] border-2 rounded-xl text-lg font-thin font-mono capitalize transition-all duration-100 hover:scale-105"
+                  className=" w-[20rem] h-[4rem] border-2 rounded-xl text-lg font-thin  capitalize transition-all duration-100 hover:scale-105"
                   onClick={setOnClose}
                 >
                   Add To Favorites
                 </button>
+
                 {/* shopping cart and favorites  end */}
               </div>
-              <div className="w-full flex flex-col justify-center  row-start-1 xl:row-start-3 row-span-1">
+              <div className="w-full flex flex-col justify-center  row-start-1 xl:row-start-3 row-p-1">
                 <FilterPopUpMenu label="product rating" Component={() => {}} />
                 <FilterPopUpMenu label="see reviews" Component={() => {}} />
                 <FilterPopUpMenu label="write a review" Component={() => {}} />
+              </div>
+              <div className="flex justify-center items-center row-start-4 row-span-1">
+                <button
+                  type="button"
+                  className=" w-[8rem] h-[4rem] border-2 rounded-xl text-lg font-thin  capitalize transition-all duration-100 hover:scale-105"
+                  onClick={setOnClose}
+                >
+                  return
+                </button>
               </div>
             </div>
             {/* inner content end */}
           </div>
           {/* end column right */}
-
-          {/* button close Modal */}
-          <div className="absolute  bottom-8 hidden  left-4 xl:flex justify-center items-center transition-all duration-175 ease-in-out ">
-            <button
-              type="button"
-              className=" w-[8rem] h-[4rem] border-2 rounded-xl text-lg font-thin font-mono capitalize transition-all duration-100 hover:scale-105"
-              onClick={setOnClose}
-            >
-              return
-            </button>
-          </div>
-          {/* end button close Modal */}
         </div>
         {/* inner content frame */}
       </div>
