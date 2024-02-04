1c77fb1e989c5724c02308e99afd6f752dfeb8fb
msharifHub <msharifemail@gmail.com>
2024-02-03 20:51:06 -0800

    #1.9 change the stylesheet writereviews tailwindcss

--- src/components/products/components/WriteReview.jsx
+++ src/components/products/components/WriteReview.jsx
@@ -25,31 +25,12 @@ export default function WriteReview({ addReview }) {
   };
 
   return (
-    <div className="mt-2 ml-8 ">
+    <div className="m-4">
       <form
         onSubmit={handleOnSubmit}
         className="flex flex-col justify-around items-start"
       >
-        <label className="flex flex-col  capitalize text-lg">
-          name:
-          <input
-            type="text"
-            value={reviewer}
-            onChange={(e) => setReviewer(e.target.value)}
-            className="border-none outline-none  focus:outline-none focus:ring-0  "
-          />
-        </label>
-        <label className="flex flex-col capitalize text-lg">
-          comment:
-          <textarea
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            required
-            className="flex justify-start border-none outline-none ring-0 ring-transparent resize-none focus:outline-none focus:ring-0"
-            maxLength={400}
-          />
-        </label>
-        <label className="flex items-center capitalize text-lg">
+        <label className="flex  justify-center items-center capitalize text-lg mb-4  ">
           <div>
             {[...Array(5)].map((_, index) => {
               const value = index + 1;
@@ -67,19 +48,39 @@ export default function WriteReview({ addReview }) {
 
                   <FontAwesomeIcon
                     icon={faStar}
-                    size="sm"
+                    size="lg"
                     color={productRating >= value ? "#444444" : "#D9D9D9"}
-                    className="cursor-pointer"
+                    className="cursor-pointer space-x-8"
                   />
                 </label>
               );
             })}
           </div>
         </label>
-        <label>
+        <label className="flex capitalize text-lg">
+          <input
+            type="text"
+            value={reviewer}
+            onChange={(e) => setReviewer(e.target.value)}
+            className=" capitalize rounded-lg  h-8 outline-none  focus:outline-none focus:ring-0  text-lg font-thin "
+            placeholder="enter name"
+          />
+        </label>
+        <label className="flex  capitalize text-lg mt-4">
+          <textarea
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            required
+            className=" flex justify-start h-12  rounded-lg outline-none ring-0 ring-transparent resize-none focus:outline-none focus:ring-0 text-lg"
+            placeholder="Type your review"
+            maxLength={200}
+          />
+        </label>
+
+        <label className="mt-4">
           <button
             type="submit"
-            className=" flex justify-center mt-2 items-center w-[8rem] h-[2rem] border-2 rounded-xl text-lg font-thin  capitalize transition-all duration-100 hover:scale-105"
+            className=" flex justify-center mt-2 items-center w-[8rem] h-[3rem] border-2 rounded-xl text-lg text-white bg-black font-thin  capitalize transition-all duration-100 hover:scale-105"
           >
             submit
           </button>
