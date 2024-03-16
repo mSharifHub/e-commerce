3d1e5648eb2f4ab492d7a9a010f0bd7019fa4cac
mSharifHub <msharifemail@gmail.com>
2024-03-15 20:57:17 -0700

    refractor code for chekEmail and entering email code UI

--- src/components/pages/SignInAndSignOut/CheckEmail.jsx
+++ src/components/pages/SignInAndSignOut/CheckEmail.jsx
@@ -4,9 +4,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { useRef, useState, useEffect, useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 import { useUser } from "../../../providers/contexts/userContext";
-import { reusePort } from "../../../helpers/modal_helpers/reusePort";
 
 export function CheckEmail() {
   const [mouseOver, setMouseOver] = useState(false);
@@ -75,80 +73,76 @@ export function CheckEmail() {
   const label = touched && !valid ? "Required" : "Email";
 
   return (
-    <>
-      <div className="relative  flex flex-col justify-start items-center w-screen  h-screen p-4 overflow-hidden">
-        <div className="text-3xl font-light flex justify-center items-center mt-[4rem] mx-auto text-slate-600 whitespace-normal  max-w-md  signInSignOutslideIn ">
-          Enter your email to join us or sign in.
-        </div>
-        <form
-          type="submit"
-          className="max-w-full mx-auto mt-[4rem] space-y-5 signInSignOutslideIn "
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <div className="relative">
-            {(mouseOver || (touched && !valid)) && (
-              <span
-                className={`absolute -top-2  -translate-y-1/8  left-4 font-thing z-4 bg-white transition-all duration-100 ease-in-out tracking-[0.25em] ${
-                  touched && !valid ? "text-red-500" : "text-gray-700"
-                }`}
-              >
-                {label}
-              </span>
-            )}
-
-            <label
-              htmlFor="email"
-              className="text-md font-medium  text-gray-700 capitalize "
-            />
-            <input
-              ref={emailRef}
-              type="email"
-              id="email"
-              name="email"
-              onMouseOver={handleOnMouseOver}
-              onMouseLeave={handleOnMouseLeave}
-              onChange={handleEmailChange}
-              value={email}
-              required
-              placeholder={!mouseOver && "Email"}
-              className={`mt-1 p-2 w-full border rounded-md shadow-sm focus:outline-none  ${
-                touched && !valid
-                  ? "focus:ring-red-500"
-                  : "focus:ring-indigo-500"
-              } focus:border-transparent`}
-            />
-          </div>
-          {email.length > 0 ? (
-            <span
-              className={`absolute top-7 text-sm ${
-                !valid ? "text-red-500" : "text-indigo-500"
-              } `}
-            >
-              {errmsg}
-            </span>
-          ) : null}
-
-          <div className="flex justify-start items-center font-thin text-slate-600 whitespace-normal text-sm">
-            <span className="mt-4">
-              By continuing, I agree to Privacy Policy and Terms of Use.
-            </span>
-          </div>
-        </form>
-        <Link to="/">
-          <button
-            type="button"
-            className="absolute  bottom-10 left-10 justify-center items-center w-[8rem] h-[3rem] mt-4 text-black border-2 rounded-lg text-lg font-thin  capitalize transition-all duration-100 ease-out  cursor-pointer hover:scale-110"
-          >
-            return
-          </button>
-        </Link>
+    <div className="relative  flex flex-col justify-start items-center w-screen  h-screen p-4 overflow-hidden">
+      <div className="text-3xl font-light flex justify-center items-center mt-[4rem] mx-auto text-slate-600 whitespace-normal  max-w-md  signInSignOutslideIn ">
+        Enter your email to join us or sign in.
       </div>
-      {submitemail &&
-        reusePort(
-          <span className="absolute  top-[13rem] right-[12rem]  z-50 flex justify-center items-center ">
-            <span className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-neutral-700"></span>
-          </span>,
-        )}
-    </>
+      <form
+        type="submit"
+        className=" max-w-full mx-auto mt-[4rem] space-y-5 signInSignOutslideIn "
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <div className="relative">
+          {(mouseOver || (touched && !valid)) && (
+            <span
+              className={`absolute -top-2  -translate-y-1/8  left-4 font-thing z-4 bg-white transition-all duration-100 ease-in-out tracking-[0.25em] ${
+                touched && !valid ? "text-red-500" : "text-gray-700"
+              }`}
+            >
+              {label}
+            </span>
+          )}
+
+          <label
+            htmlFor="email"
+            className="text-md font-medium  text-gray-700 capitalize "
+          />
+          {submitemail && (
+            <span className="absolute  -top-10 inset-x-0  z-50 flex justify-center items-center ">
+              <span className="animate-spin rounded-full h-[8rem] w-[8rem] border-t-2 border-b-2 border-neutral-700"></span>
+            </span>
+          )}
+
+          <input
+            ref={emailRef}
+            type="email"
+            id="email"
+            name="email"
+            onMouseOver={handleOnMouseOver}
+            onMouseLeave={handleOnMouseLeave}
+            onChange={handleEmailChange}
+            value={email}
+            required
+            placeholder={!mouseOver && "Email"}
+            className={`mt-1 p-2 w-full border rounded-md shadow-sm focus:outline-none  ${
+              touched && !valid ? "focus:ring-red-500" : "focus:ring-indigo-500"
+            } focus:border-transparent`}
+          />
+        </div>
+        {email.length > 0 ? (
+          <span
+            className={`absolute top-7 text-sm ${
+              !valid ? "text-red-500" : "text-indigo-500"
+            } `}
+          >
+            {errmsg}
+          </span>
+        ) : null}
+
+        <div className=" flex justify-start items-center font-thin text-slate-600 whitespace-normal text-sm">
+          <span className="mt-4">
+            By continuing, I agree to Privacy Policy and Terms of Use.
+          </span>
+        </div>
+      </form>
+      <Link to="/">
+        <button
+          type="button"
+          className="absolute  bottom-10 left-10 justify-center items-center w-[8rem] h-[3rem] mt-4 text-black border-2 rounded-lg text-lg font-thin  capitalize transition-all duration-100 ease-out  cursor-pointer hover:scale-110"
+        >
+          return
+        </button>
+      </Link>
+    </div>
   );
 }
--- src/components/pages/SignInAndSignOut/TwoWayAuthentication.jsx
+++ src/components/pages/SignInAndSignOut/TwoWayAuthentication.jsx
@@ -4,11 +4,10 @@
 /* eslint-disable jsx-a11y/label-has-for */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/mouse-events-have-key-events */
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useUser } from "../../../providers/contexts/userContext";
 import ResetButton from "./CodeResetButton";
-import { reusePort } from "../../../helpers/modal_helpers/reusePort";
 
 export default function TwoWayAuthentication() {
   const { state: userState, dispatch } = useUser();
@@ -20,9 +19,21 @@ export default function TwoWayAuthentication() {
   const [code, setCode] = useState(Array(5).fill("")); // code as array of lenght 5
   const navigate = useNavigate(); // to move to next page
 
-  const dummyCode = [1, 2, 3, 4, 5]; // simulate code back end
+  // User ref to focus on input
+  const userRef = useRef(null);
 
-  // use effect to control timer
+  // Use Effect input to focus on input
+  useEffect(() => {
+    if (userRef.current) {
+      userRef.current.focus();
+    }
+  }, []);
+
+  // TODO remove after implementing back end
+  const dummyCode = [1, 2, 3, 4, 5]; // simulate code back end
+  const isFilled = code.every((char) => char !== "");
+
+  // Use Effect to control timer
   useEffect(() => {
     let interval;
     if (isWaitingForReset && timer > 0) {
@@ -36,7 +47,7 @@ export default function TwoWayAuthentication() {
     return () => clearInterval(interval);
   }, [isWaitingForReset, timer]);
 
-  // use effect to  move to register or login
+  // Use Effect to  move to register or login
   useEffect(() => {
     if (codeValid) {
       const redirect = setTimeout(() => {
@@ -47,29 +58,55 @@ export default function TwoWayAuthentication() {
     }
   }, [codeValid, navigate]);
 
+  // Use Effect to clear input if input is not valid
+  useEffect(() => {
+    let timeout;
+    if (isFilled && !codeValid) {
+      timeout = setTimeout(() => {
+        setCode(Array(5).fill(""));
+        setInputValidity(Array(5).fill(false));
+
+        if (userRef.current) {
+          userRef.current.focus();
+        }
+      }, 800);
+    }
+    return () => clearTimeout(timeout);
+  }, [codeValid, isFilled]);
+
+  // Use Effect to clear inputs after user inputs valid input
+  useEffect(() => {
+    let timeout;
+    if (isFilled && !codeValid) {
+      timeout = setTimeout(() => {
+        setCode(Array(5).fill(""));
+        setInputValidity(Array(5).fill(false));
+      }, 1000);
+    }
+    return () => clearTimeout(timeout);
+  }, [isFilled, codeValid]);
+
   const handleCodeInput = (index, value) => {
     // User touched the input
     setTouchedCodeInput(true);
 
     // Update the code with the new value entered by the user
     const newCode = [...code];
+
     newCode[index] = value;
 
-    setCode(newCode);
-
+    const isValidInput = value.toString() === dummyCode[index].toString();
     const newUserInputValidity = [...inputValidity];
-    newUserInputValidity[index] =
-      value.toString() === dummyCode[index].toString();
+    newUserInputValidity[index] = isValidInput;
 
+    setCode(newCode);
     setInputValidity(newUserInputValidity);
-
-    if (value && index < 4) {
+    if (value && index < code.length - 1) {
       const nextInput = document.getElementById(`code-${index + 1}`);
       if (nextInput) {
         nextInput.focus();
       }
     }
-
     const allInputsAreValid = newUserInputValidity.every(Boolean);
 
     setCodeValid(allInputsAreValid);
@@ -77,12 +114,12 @@ export default function TwoWayAuthentication() {
 
   return (
     <>
-      <div className="relative  flex flex-col justify-start items-center w-screen  h-screen p-4 overflow-hidden signInSignOutslideIn">
+      <div className="relative flex flex-col justify-start items-center w-screen  h-screen p-4 overflow-hidden signInSignOutslideIn">
         {/* title */}
-        <div className="flex flex-col ">
-          <div className=" flex justify-center items-center text-3xl font-light   mt-[4rem] mx-auto text-slate-600 whitespace-normal   ">
+        <div className="flex  flex-col">
+          <span className="flex justify-center items-center text-3xl font-light  mt-[4rem]  text-slate-600  whitespace-nowwrap sm:whitespace-normal  ">
             For your security let&apos;s authenticate your email.
-          </div>
+          </span>
 
           <span className="flex flex-col justify-end items-center w-full text-sm mt-4">
             We&apos;ve sent a code to:
@@ -95,10 +132,11 @@ export default function TwoWayAuthentication() {
           </span>
         </div>
 
-        <form className="w-full mx-auto mt-4 space-y-5 ">
-          <div className=" flex justify-center space-x-10 mt-4">
+        <form className="w-full px-2">
+          <div className="flex justify-center flex-wrap gap-4 mt-4">
             {code.map((char, index) => (
               <input
+                ref={index === 0 ? userRef : null} // focus on mount only on first index
                 key={index}
                 type="text"
                 id={`code-${index}`}
@@ -107,15 +145,12 @@ export default function TwoWayAuthentication() {
                 value={char}
                 onChange={(e) => handleCodeInput(index, e.target.value)}
                 required
-                className={`w-[4rem] h-[4rem] border rounded-md text-center focus:outline-none  capitalize ${
-                  touchedCodeInput && !inputValidity[index]
-                    ? "focus:ring-red-500"
-                    : "focus:ring-indigo-500"
-                } focus:border-transparent`}
+                className="w-[4rem] h-[4rem] border rounded-md text-center focus:outline-none  capitalize 
+                  focus:border-transparent"
               />
             ))}
             <span
-              className={`flex justify-center items-center transition-transform duration-300 ease-out rotate:0  ${
+              className={`flex justify-center items-center transition-transform duration-300 ease-out rotate:0 mt-4 sm:mt-0  sm:ml-4 ${
                 !isWaitingForReset ? "hover:rotate-180" : ""
               }`}
             >
@@ -131,12 +166,15 @@ export default function TwoWayAuthentication() {
             </span>
           )}
         </form>
-        {code.every((char) => char !== "" && !codeValid) ? (
-          <span className="flex justify-center items-center mt-2">
-            code is not valid
+        {isFilled && (
+          <span
+            className={`flex justify-center items-center mt-2 ${
+              codeValid ? "text-blue-500" : "text-red-500"
+            }`}
+          >
+            code is {codeValid ? "valid" : "not valid"}
           </span>
-        ) : null}
-        {codeValid && reusePort(<span> </span>)}
+        )}
       </div>
 
       {/* return to main page  button */}
