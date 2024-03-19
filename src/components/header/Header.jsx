649f57edd53a8c9fe975b2c427e0e8e038cd35b3
mSharifHub <msharifemail@gmail.com>
2024-03-18 17:18:36 -0700

    header pop up menu modal

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,62 +1,99 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/self-closing-comp */
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useUser } from "../../providers/contexts/userContext";
-import useSignOut from "../pages/SignInAndSignOut/helpers/signOut";
-import { reusePort } from "../../helpers/modal_helpers/reusePort";
-import Loading from "../navigation/icons/Loading";
 import UserIcon from "../navigation/icons/User";
+import ModalMenu from "./components/ModalMenu";
 
 export default function Header() {
-  const [isSignOut, setSignOut] = useState(false);
   const { state } = useUser();
-  const signOut = useSignOut(() => setSignOut(false));
+  const [isLinkHelpHovered, setIsLinkHelpHovered] = useState(false);
+  const [isModalHelpHovered, setIsModalHelpHovered] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-  const navigationLinks = [
-    { name: "Find a Store", path: "/api/find-store" },
-    { name: "Help", path: "/api/help" },
-    { name: "Join Us", path: "/api/join-us" },
-  ];
-  const len = navigationLinks.length - 1;
+  const navigationOptions = {
+    help: {
+      title: "Help",
+      links: [
+        { name: "Order Help", path: "#" },
+        { name: "Tracking Help", path: "#" },
+        { name: "Issue with an Order", path: "#" },
+        { name: "Returns", path: "#" },
+        { name: "Send us feefback", path: "#" },
+        { name: "Contact us", path: "#" },
+      ],
+    },
+    user: {
+      title: "Account",
+      links: [
+        { name: "Profile", path: "#" },
+        { name: "Orders", path: "#" },
+        { name: "Favorites", path: "#" },
+        { name: "Inbox", path: "#" },
+        { name: "Experiences", path: "#" },
+        { name: "Account Settings", path: "#" },
+        { name: "Log Out", path: "#" },
+      ],
+    },
+  };
+
+  // Handle mouse transaction
+  useEffect(() => {
+    let timer;
+    if (isLinkHelpHovered || isModalHelpHovered) {
+      setShowModal(true);
+    } else {
+      timer = setTimeout(() => {
+        setShowModal(false);
+      }, 100);
+    }
+    return () => clearTimeout(timer);
+  }, [isLinkHelpHovered, isModalHelpHovered]);
 
   return (
-    <>
-      <div className=" flex justify-end items-center bg-neutral-100 px-4 py-2 ">
-        <nav className="flex justify-center items-center space-x-4  mx-4 text-sm">
-          {navigationLinks.map((link, index) => (
-            <React.Fragment key={link.name}>
-              <Link to={link.path} className="px-2">
-                {link.name}
-              </Link>
-              {(index < len || index === len) && (
-                <span className="px-2">|</span>
-              )}
-            </React.Fragment>
-          ))}
-          {state.isLoggedIn ? (
-            <div className="flex justify-around items-center space-x-4">
-              <span className="hidden md:flex col-start-1 col-span-1  justify-start items-center text-sm">
-                welcome, {state.userEmail}
-              </span>
-              <span>
-                <UserIcon />
-              </span>
+    <div className=" flex justify-end items-center bg-neutral-100 px-4 py-2">
+      <nav className="flex items-center space-x-2 mx-4 text-sm ">
+        <Link to="/api/find-store" className="px-2">
+          Find a Store
+        </Link>
+        <span className="px-2">|</span>
+        <div className="relative">
+          <Link
+            to="/api/help"
+            className="px-2"
+            onMouseEnter={() => setIsLinkHelpHovered(true)}
+            onMouseLeave={() => setIsLinkHelpHovered(false)}
+          >
+            Help
+          </Link>
+          {showModal && (
+            <div
+              className="absolute top-7 -right-4 z-50"
+              onMouseEnter={() => setIsModalHelpHovered(true)}
+              onMouseLeave={() => setIsModalHelpHovered(false)}
+            >
+              <ModalMenu
+                title={navigationOptions.help.title}
+                links={navigationOptions.help.links}
+              />
             </div>
-          ) : (
-            <Link to="api/credentials/check-email"> Sign In</Link>
           )}
-        </nav>
-      </div>
-      {/* {isSignOut &&
-        reusePort(
-          <div className=" absolute top-[2rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2   bg-white  w-[20rem] h-[4rem]  z-50 flex  flex-col justify-center items-center  rounded-lg">
-            <span className="flex capitalize  text-md font-semibold mb-2">
-              logging out
+        </div>
+        <span className="px-2">|</span>
+        {state.isLoggedIn ? (
+          <div className="relative flex justify-around items-center space-x-2 cursor-pointer">
+            <span className="hidden md:flex col-start-1 col-span-1 justify-start items-center text-sm transition-transform duration-100 hover:opacity-50">
+              Welcome, {state.userEmail}
             </span>
-            <Loading />
-          </div>,
-        )} */}
-    </>
+            <span className="inline-flex justify-center items-center rounded-full h-10 w-10 transition-colors duration-175 ease-out hover:bg-slate-200">
+              <UserIcon />
+            </span>
+          </div>
+        ) : (
+          <Link to="/api/credentials/check-email"> Sign In</Link>
+        )}
+      </nav>
+    </div>
   );
 }
--- src/helpers/hooks/UseVisibility.jsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export default function UseVisibility(initialState = true) {
-  const [isVisible, setIsVisible] = useState(initialState);
-
-  return [isVisible, setIsVisible];
-}
