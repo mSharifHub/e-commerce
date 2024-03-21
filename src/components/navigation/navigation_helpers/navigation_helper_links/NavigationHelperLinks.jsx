c7b4e7a72b659edca2043c86a7f44e2dc197bcad
mSharifHub <msharifemail@gmail.com>
2024-03-21 01:44:39 -0700

    implemented navigation helper modal links

--- src/components/navigation/navigation_helpers/navigation_helper_links/NavigationHelperLinks.jsx
+++ src/components/navigation/navigation_helpers/navigation_helper_links/NavigationHelperLinks.jsx
@@ -1,17 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { navigationHelperRoutes } from "../../../../styles/routes/routes";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import NavigationHelperModal from "../NavigationHelperModal";
+import { navigationHeaderRoutes } from "../../../../styles/routes/routes";
+import { reusePort } from "../../../../helpers/modal_helpers/reusePort";
 
 export default function NavigationHelperLinks() {
+  const sections = ["news_feature", "men", "women", "kids"];
+  const [activeModal, setActiveModal] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [isSectionHovered, setIsSectionHovered] = useState(false);
+  const [isModalHovered, setIsModalHovered] = useState(false);
+
+  useEffect(() => {
+    let modalTimer;
+
+    if (isSectionHovered || isModalHovered) {
+      setShowModal(true);
+    } else {
+      modalTimer = setTimeout(() => {
+        setShowModal(false);
+        setActiveModal(null);
+      }, 100);
+    }
+    return () => clearTimeout(modalTimer);
+  }, [isSectionHovered, isModalHovered]);
+
   return (
-    <nav className="flex space-x-[2rem]   ">
-      {navigationHelperRoutes.map((link) => (
-        <React.Fragment key={link.name}>
-          <Link to={link.path}>
-            <div className="text-slate-600  font-semibold  ">{link.name}</div>
-          </Link>
-        </React.Fragment>
+    <nav className="flex space-x-[2rem]">
+      {sections.map((section) => (
+        <div
+          key={section}
+          onMouseEnter={() => {
+            setActiveModal(section);
+            setIsSectionHovered(true);
+          }}
+          onMouseLeave={() => {
+            setIsSectionHovered(false);
+          }}
+          className="relative"
+        >
+          <span className="cursor-pointer capitalize">
+            {section.replace("_", " ")}
+          </span>
+        </div>
       ))}
+      {showModal &&
+        activeModal &&
+        reusePort(
+          <div
+            onMouseEnter={() => setIsModalHovered(true)}
+            onMouseLeave={() => setIsModalHovered(false)}
+            className="absolute top-[5rem]  w-full h-1/4 bg-white z-50"
+          >
+            <NavigationHelperModal
+              title={navigationHeaderRoutes[activeModal]?.title}
+              links={navigationHeaderRoutes[activeModal]?.links}
+            />
+          </div>,
+        )}
     </nav>
   );
 }
--- src/styles/routes/routes.js
+++ src/styles/routes/routes.js
@@ -1,11 +1,3 @@
-export const navigationHelperRoutes = [
-  { name: "New & Featured", path: "api/product-display" },
-  { name: "Men", path: "api/product-display" },
-  { name: "Women", path: "api/product-display" },
-  { name: "Kids", path: "api/product-display" },
-  { name: "Sales", path: "api/product-display" },
-];
-
 export const navigationHeaderRoutes = {
   help: {
     title: "Help",
@@ -28,6 +20,40 @@ export const navigationHeaderRoutes = {
       { name: "Experiences", path: "#" },
       { name: "Account Settings", path: "#" },
       { name: "Log Out", path: "#" },
+    ],
+  },
+
+  news_feature: {
+    title: "Features",
+    links: [
+      { name: "Shop all new arrivals", path: "#" },
+      { name: "Best Sellers", path: "#" },
+      { name: "Memeber Shop", path: "#" },
+    ],
+  },
+
+  men: {
+    title: "Men",
+    links: [
+      { name: "Shop men new arrivals", path: "#" },
+      { name: "Men best sellers", path: "#" },
+      { name: "Men on sale items", path: "#" },
+    ],
+  },
+  women: {
+    title: "Women",
+    links: [
+      { name: "Shop women new arrivals", path: "#" },
+      { name: "Women best sellers", path: "#" },
+      { name: "Women on sale items", path: "#" },
+    ],
+  },
+  kids: {
+    title: "Kids",
+    links: [
+      { name: "Shop kids new arrivals", path: "#" },
+      { name: "Kids best sellers", path: "#" },
+      { name: "Kids on sale items", path: "#" },
     ],
   },
 };
