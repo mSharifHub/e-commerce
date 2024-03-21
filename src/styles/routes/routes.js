c1d29775d4ee1acb0a482005c50f283890b970ec
mSharifHub <msharifemail@gmail.com>
2024-03-21 05:17:44 -0700

    added temporary link to products display temp page

--- src/styles/routes/routes.js
+++ src/styles/routes/routes.js
@@ -26,34 +26,34 @@ export const navigationHeaderRoutes = {
   news_feature: {
     title: "Features",
     links: [
-      { name: "Shop all new arrivals", path: "#" },
-      { name: "Best Sellers", path: "#" },
-      { name: "Memeber Shop", path: "#" },
+      { name: "Shop all new arrivals", path: "api/product-display" },
+      { name: "Best Sellers", path: "api/product-display" },
+      { name: "Memeber Shop", path: "api/product-display" },
     ],
   },
 
   men: {
     title: "Men",
     links: [
-      { name: "Shop men new arrivals", path: "#" },
-      { name: "Men best sellers", path: "#" },
-      { name: "Men on sale items", path: "#" },
+      { name: "Shop men new arrivals", path: "api/product-display" },
+      { name: "Men best sellers", path: "api/product-display" },
+      { name: "Men on sale items", path: "api/product-display" },
     ],
   },
   women: {
     title: "Women",
     links: [
-      { name: "Shop women new arrivals", path: "#" },
-      { name: "Women best sellers", path: "#" },
-      { name: "Women on sale items", path: "#" },
+      { name: "Shop women new arrivals", path: "api/product-display" },
+      { name: "Women best sellers", path: "api/product-display" },
+      { name: "Women on sale items", path: "api/product-display" },
     ],
   },
   kids: {
     title: "Kids",
     links: [
-      { name: "Shop kids new arrivals", path: "#" },
-      { name: "Kids best sellers", path: "#" },
-      { name: "Kids on sale items", path: "#" },
+      { name: "Shop kids new arrivals", path: "api/product-display" },
+      { name: "Kids best sellers", path: "api/product-display" },
+      { name: "Kids on sale items", path: "api/product-display" },
     ],
   },
 };
